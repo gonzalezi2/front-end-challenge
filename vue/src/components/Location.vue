473c7dca190672b1c8bb5cb51b4824add079b218
Ivan <ivan.gonzalez@gmail.com>
2018-08-24 19:05:27 -0400

    added new functions for stats and refactored

--- vue/src/components/Location.vue
+++ vue/src/components/Location.vue
@@ -16,7 +16,6 @@
                                 {{location.name}}
                         </option>
                     </select>
-                    {{busiestWeekdays}}
                     </div>
                     <div class="col-2">
                         <button type="button" class="close" aria-label="Close" v-on:click="$emit('delete-widget', widget)">
@@ -27,8 +26,8 @@
             </div>
             <div class="card-body">
                 <p>Busiest Weekday(s): <strong>{{commaDays}} </strong></p>
-                <p>Most Popular Agreement: <strong>{{mostPopularAgreement}}</strong></p>
-                <p>Most Popular Agreement on Busiest Weekday: <strong>{{mpaOnBusiestWeekday}}</strong></p>
+                <p>Most Popular Agreement: <strong>{{commAgreements}}</strong></p>
+                <p>Most Popular Agreement on Busiest Weekday: <strong>{{commMpaAgreements}}</strong></p>
             </div>
         </div>
     </div>
@@ -46,17 +45,23 @@ export default {
     data: function() {
         return {
             locationCheckins: [],
-            locationAgreements: [],
+            memberAgreements: [],
             busiestWeekdays: [],
             agreementTypes: {},
-            mostPopularAgreement: "",
-            mpaOnBusiestWeekday: "",
+            mostPopularAgreement: [],
+            mpaOnBusiestWeekday: [],
             selected: Number
         }
     },
     computed: {
         commaDays: function() {
             return this.busiestWeekdays.join(" ").split(" ").join(", ");
+        },
+        commAgreements: function() {
+            return this.mostPopularAgreement.join(" ").split(" ").join(", ");
+        },
+        commMpaAgreements: function() {
+            return this.mpaOnBusiestWeekday.join(" ").split(" ").join(", ");
         }
     },
     watch: {
@@ -65,6 +70,8 @@ export default {
             handler() {
                 this.filterCheckins();
                 this.findBusiestWeekday();
+                this.findMostPopularAgreement();
+                this.findWeekdayAgreements();
             }
         }
     },
@@ -77,45 +84,86 @@ export default {
                 this.$http.get(checkinsURL), this.$http.get(agreementsURL)
             ]).then(([checkins, agreements]) => {
                 this.locationCheckins = checkins.body.data;
-                this.locationAgreements = agreements.body.data;
+                this.memberAgreements = agreements.body.data;
                 this.agreementTypes = agreements.body.metadata.agreementTypes;
             })
-            .then(this.filterCheckins)
-            .then(this.findBusiestWeekday);
+            //.then(this.filterCheckins)
+            .then(this.findBusiestWeekday)
+            .then(this.findMostPopularAgreement)
+            .then(this.findWeekdayAgreements);
         },
-        filterCheckins: function() {
+        filterCheckins: function() {            
             this.locationCheckins = this.locationCheckins.filter((checkin, index) => {
-                return checkin.date >= this.dates.startDate && checkin.date <= this.dates.endDate;
+                return checkin.date >= this.dates.startDate && checkin.date <= (this.dates.endDate || new Date());
             });
         },
+        findMode: function(arr) {
+            var mappedCounts = new Map();
+            var current = "";
+            var counter;
+            var mode = [];
+
+            // Sorts the array to make counting easier
+            arr.sort().forEach(item => {
+                // increases counter for all non-unique items
+                current === item ? counter++ : counter = 1;
+                // sets item as new key on map or increases its count
+                mappedCounts.set(item, counter);
+                current = item;
+            });
+            // Takes mappedCounts from map object to sort in descending order
+            var sortedCounts = Array.from(mappedCounts.values()).sort((a, b) => b - a);
+
+            // Takes the most occurring instance after the sort of the array and pushes
+            // all map values into the mode array to get the mode
+            for (const [key, value] of mappedCounts) {
+                if(value === sortedCounts[0]) {
+                    mode.push(key);
+                }
+            }
+            return mode;        
+        },
         findBusiestWeekday: function() {
-            var weekday = [];
-            var dayCounts = new Map();
-            var currentDay = "";
-            var counter = 1;
-            var highestDays = [];
+            let weekday = [];
             //Pushes a day-formatted string into the weekday array
             this.locationCheckins.forEach(checkin => {
                 weekday.push(moment(checkin.date).format('dddd'));
             });
-            // Sorts the weekday array for counting, and resets counter on new day
-            weekday.sort().forEach(day => {
-                currentDay === day ? counter++ : counter = 1;
-                dayCounts.set(day, counter);
-                currentDay = day;
+            this.busiestWeekdays = this.findMode(weekday);
+        },
+        findMostPopularAgreement: function() {
+            let agreements = [];
+            this.memberAgreements.forEach(item => {
+                agreements.push(this.agreementTypes[item.agreement]);
             });
-            // Takes daycounts from map object to sort in descending order
-            var sortedDayCounts = Array.from(dayCounts.values()).sort((a, b) => b - a);
+            this.mostPopularAgreement = this.findMode(agreements);
+        },
+        findWeekdayAgreements: function() {
+            let popularWeekdays = [];
+            let memberIds = new Set();
+            let agreements = [];
+            
+            this.busiestWeekdays.forEach(day => {
+                // Filters location check-ins to just those check-ins from the busiest days
+                popularWeekdays = this.locationCheckins.filter(function(loc){
+                    return moment(loc.date).format('dddd') === day;
+                });
+            });
 
-            // Takes the highest checking count and pushes all days
-            // that have the same number of checkins
-            for (const [key, value] of dayCounts) {
-                if(value === sortedDayCounts[0]) {
-                    highestDays.push(key);
-                }
-            }
-            console.log(this.busiestWeekdays, highestDays);
-            this.busiestWeekdays = highestDays;
+            // Gets all the member ids on the filtered weekdays
+            popularWeekdays.forEach(day => {
+                memberIds.add(day.memberId);
+            });
+
+            // Loop through memberIds and get the agreements
+            memberIds.forEach(id => {
+                this.memberAgreements.forEach(member => {
+                    if(member.memberId === id) {
+                        agreements.push(this.agreementTypes[member.agreement]);
+                    }
+                });
+            });
+            this.mpaOnBusiestWeekday = this.findMode(agreements);
         }
     }
 }
